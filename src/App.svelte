61019a470376c45f372419e47dd539993408357a
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2020-10-04 18:54:40 +0100

    :sparkles: Initial set up

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	const socket = new WebSocket('ws://localhost:8999');
+
+	socket.addEventListener("message", (data) => {
+		const message = JSON.parse(data.data).data;
+	});
+
+</script>
+
+<main>
+	<h1>Hello World!</h1>
+</main>
+
+<style>
+
+</style>
