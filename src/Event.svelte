bc7c255762d79808032fd5fda2367df0400459b6
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2020-10-04 21:09:11 +0100

    :sparkles: Progress on message events styles

--- src/Event.svelte
+++ src/Event.svelte
@@ -1,13 +1,76 @@
 <script lang="ts">
 import type { ChatMessageData } from './types'
-
 export let event: ChatMessageData;
-
 
 </script>
 
-<style></style>
+<style>
+  :root {
+    --top-border-size: 6px;
+  }
 
-<div class="event">
-  {event.message}
+  .event {
+    background-color: var(--black);
+    display: flex;
+    flex-direction: row;
+    width: 400px;
+    margin-top: 1rem;
+    border-top: var(--top-border-size) solid;
+    border-image-slice: 1;
+  }
+
+  .event-vip {
+    border-image-source: linear-gradient(90deg, var(--black) 0%, var(--vip) 100%);
+  }
+
+  .event-mod {
+    border-image-source: linear-gradient(90deg, var(--black) 0%, var(--mod) 100%);
+  }
+
+  .event-broadcaster {
+    border-image-source: linear-gradient(90deg, var(--black) 0%, var(--broadcaster) 100%);
+  }
+
+  .displayName {
+    color: var(--white);
+    font-size: 0.9rem;
+    margin-bottom: 1rem;
+    color: var(--yellow);
+  }
+
+  .message {
+    color: var(--white);
+    font-size: 1.2rem;
+  }
+
+  .avatarContainer {
+    background-repeat: no-repeat;
+    background-position: center center;
+    background-size: cover;
+    flex: 0 0 100px;
+    background-size: 150px;
+    background-position-y: 50%; 
+    background-color: var(--black);
+  }
+
+  .messageContainer {
+    padding: 1rem;
+  }
+
+
+</style>
+
+<div class="event"
+class:event-vip="{event.isVip === true}"
+class:event-mod="{event.isMod === true}"
+class:event-broadcaster="{event.isBroadcaster === true}"
+>
+
+  <div class="avatarContainer" style={`background-image: url(${event.logoUrl});`}>
+  </div>
+
+  <div class="messageContainer">
+    <p class="displayName">{event.displayName}</p>
+    <p class="message">{event.message}</p>
+  </div>
 </div>
